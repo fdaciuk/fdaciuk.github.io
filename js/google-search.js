467a3bf69feaec470929958cddd88b4c84d3519b
Fernando Daciuk <f.daciuk@gmail.com>
2015-06-24 00:03:17 -0300

    noop

--- js/google-search.js
+++
@@ -1,9 +0,0 @@
-;(function( $ ) {
-    $(function() {
-        $( '[data-js="google-search-form"]' ).on( 'submit', function( e ) {
-            e.preventDefault();
-            var inputVal = $( '[data-js="google-search-input"]' ).val();
-            window.location.href = $( this ).attr( 'data-action' ) + ' ' + inputVal;
-        });
-    });
-})( jQuery );
